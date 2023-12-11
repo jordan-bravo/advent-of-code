dff37e7c365670717456a364c58008e7d9225cac
Jordan Bravo <jordan@bravo.cc>
2023-12-10 22:13:02 -0500

    start solving day 01

--- 2023/day-01/solution-go/cmd/day-01/main.go
+++ 2023/day-01/solution-go/cmd/day-01/main.go
@@ -1,7 +1,18 @@
 package main
 
 import "fmt"
+import "unicode"
 
 func main() {
 	fmt.Println("Hello world!")
+	// for each line, do the following:
+	// remove all letters
+	// for each character, do the following:
+	// if character is number, add to new slice
+	// for new slice:
+	// if length is one, copy number to make two
+	// if length is two, done
+	// if length is > 2, remove all but first and last
+	// finally:
+	// sum all numbers
 }
